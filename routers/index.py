0ee18a932968ff369e6f075ac4d13e1d8264576e
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2025-07-03 18:07:46 -0300

    Remoção do endpoint / da lista de docs

--- routers/index.py
+++ routers/index.py
@@ -3,6 +3,6 @@ from starlette.responses import RedirectResponse
 
 index_router = APIRouter()
 
-@index_router.get("/")
+@index_router.get("/", include_in_schema=False)
 def index():
     return RedirectResponse(url="/docs")
